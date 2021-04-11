58ae2ea57b0aaa8342f8038bb2f55db6f0bea5f0
catalinpuricoi15 <61915381+catalinpuricoi15@users.noreply.github.com>
2021-04-11 23:14:21 +0300

    worked on webside design

--- JS/clasa.js
+++ JS/clasa.js
@@ -5,6 +5,11 @@ var actions = document.getElementById("actions");
 var id = params.get('classId');
 
 document.getElementById("id").innerHTML = id  ;
+if(id == "321")
+    document.getElementById("name").innerHTML = "SGDB"  ;
+else
+    document.getElementById("name").innerHTML = "Tehnologii WEB"  ;
+
 
 function doAction(action){
     window.location.href = action + ".html" + "?username=" + username + "&classId=" + id;
